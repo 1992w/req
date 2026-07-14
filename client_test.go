8e4fc75958822e6839dabbc98013d12cf8349dc1
1992w <jiangbing_yang@163.com>
2026-07-14 21:47:52 +0800

    fix:修复 SetTLSFingerprintSpec 连续握手失败的问题

--- client_test.go
+++ client_test.go
@@ -19,6 +19,7 @@ import (
 
 	"github.com/imroc/req/v3/internal/header"
 	"github.com/imroc/req/v3/internal/tests"
+	utls "github.com/refraction-networking/utls"
 	"golang.org/x/net/publicsuffix"
 )
 
@@ -751,4 +752,87 @@ func TestCloneCookieJar(t *testing.T) {
 	c2.SetCookieJar(nil)
 	tests.AssertEqual(t, true, c2.cookiejarFactory == nil)
 	tests.AssertEqual(t, true, c2.httpClient.Jar == nil)
+}
+
+func TestSetTLSFingerprintSpec(t *testing.T) {
+	c := tc().SetTLSFingerprintSpec(func() utls.ClientHelloSpec {
+		return utls.ClientHelloSpec{
+			CipherSuites: []uint16{
+				utls.GREASE_PLACEHOLDER,
+				utls.TLS_AES_128_GCM_SHA256,
+				utls.TLS_AES_256_GCM_SHA384,
+				utls.TLS_CHACHA20_POLY1305_SHA256,
+				utls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				utls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				utls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				utls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				utls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+				utls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+				utls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+				utls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+				utls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+				utls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+				utls.TLS_RSA_WITH_AES_128_CBC_SHA,
+				utls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			},
+			CompressionMethods: []byte{
+				0x00,
+			},
+			Extensions: utls.ShuffleChromeTLSExtensions([]utls.TLSExtension{
+				&utls.UtlsGREASEExtension{},
+				&utls.SNIExtension{},
+				&utls.ExtendedMasterSecretExtension{},
+				&utls.RenegotiationInfoExtension{Renegotiation: utls.RenegotiateOnceAsClient},
+				&utls.SupportedCurvesExtension{[]utls.CurveID{
+					utls.GREASE_PLACEHOLDER,
+					utls.X25519MLKEM768,
+					utls.X25519,
+					utls.CurveP256,
+					utls.CurveP384,
+				}},
+				&utls.SupportedPointsExtension{SupportedPoints: []byte{
+					0x00,
+				}},
+				&utls.SessionTicketExtension{},
+				&utls.ALPNExtension{AlpnProtocols: []string{"h2", "http/1.1"}},
+				&utls.StatusRequestExtension{},
+				&utls.SignatureAlgorithmsExtension{SupportedSignatureAlgorithms: []utls.SignatureScheme{
+					utls.ECDSAWithP256AndSHA256,
+					utls.PSSWithSHA256,
+					utls.PKCS1WithSHA256,
+					utls.ECDSAWithP384AndSHA384,
+					utls.PSSWithSHA384,
+					utls.PKCS1WithSHA384,
+					utls.PSSWithSHA512,
+					utls.PKCS1WithSHA512,
+				}},
+				&utls.SCTExtension{},
+				&utls.KeyShareExtension{[]utls.KeyShare{
+					{Group: utls.CurveID(utls.GREASE_PLACEHOLDER), Data: []byte{0}},
+					{Group: utls.X25519MLKEM768},
+					{Group: utls.X25519},
+				}},
+				&utls.PSKKeyExchangeModesExtension{[]uint8{
+					utls.PskModeDHE,
+				}},
+				&utls.SupportedVersionsExtension{[]uint16{
+					utls.GREASE_PLACEHOLDER,
+					utls.VersionTLS13,
+					utls.VersionTLS12,
+				}},
+				&utls.UtlsCompressCertExtension{[]utls.CertCompressionAlgo{
+					utls.CertCompressionBrotli,
+				}},
+				&utls.ApplicationSettingsExtensionNew{SupportedProtocols: []string{"h2"}},
+				utls.BoringGREASEECH(),
+				&utls.UtlsGREASEExtension{},
+			}),
+		}
+	})
+	if _, err := c.R().Get("https://tls.browserleaks.com/json"); err != nil {
+		t.Errorf("TestSetTLSFingerprintSpec failed: %v", err)
+	}
+	if _, err := c.R().Get("https://tools.scrapfly.io/api/fp/ja3"); err != nil {
+		t.Errorf("TestSetTLSFingerprintSpec failed: %v", err)
+	}
 }
