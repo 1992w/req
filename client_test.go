8bd83b5790946d2cd2d2607fb3209861a82a45b5
1992w <jiangbing_yang@163.com>
2026-07-22 23:58:58 +0800

    fix: refactor TestSetTLSFingerprintSpec to use local test servers and fix go vet

--- client_test.go
+++ client_test.go
@@ -10,6 +10,7 @@ import (
 	"net"
 	"net/http"
 	"net/http/cookiejar"
+	"net/http/httptest"
 	"net/url"
 	"os"
 	"regexp"
@@ -755,84 +756,46 @@ func TestCloneCookieJar(t *testing.T) {
 }
 
 func TestSetTLSFingerprintSpec(t *testing.T) {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}
+	server1 := httptest.NewTLSServer(http.HandlerFunc(handler))
+	defer server1.Close()
+
 	c := tc().SetTLSFingerprintSpec(func() utls.ClientHelloSpec {
 		return utls.ClientHelloSpec{
 			CipherSuites: []uint16{
-				utls.GREASE_PLACEHOLDER,
 				utls.TLS_AES_128_GCM_SHA256,
-				utls.TLS_AES_256_GCM_SHA384,
-				utls.TLS_CHACHA20_POLY1305_SHA256,
-				utls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 				utls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-				utls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-				utls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				utls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-				utls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-				utls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-				utls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-				utls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-				utls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-				utls.TLS_RSA_WITH_AES_128_CBC_SHA,
-				utls.TLS_RSA_WITH_AES_256_CBC_SHA,
 			},
-			CompressionMethods: []byte{
-				0x00,
-			},
-			Extensions: utls.ShuffleChromeTLSExtensions([]utls.TLSExtension{
-				&utls.UtlsGREASEExtension{},
+			CompressionMethods: []byte{0x00},
+			Extensions: []utls.TLSExtension{
 				&utls.SNIExtension{},
-				&utls.ExtendedMasterSecretExtension{},
-				&utls.RenegotiationInfoExtension{Renegotiation: utls.RenegotiateOnceAsClient},
-				&utls.SupportedCurvesExtension{[]utls.CurveID{
-					utls.GREASE_PLACEHOLDER,
-					utls.X25519MLKEM768,
+				&utls.SupportedCurvesExtension{Curves: []utls.CurveID{
 					utls.X25519,
 					utls.CurveP256,
-					utls.CurveP384,
 				}},
-				&utls.SupportedPointsExtension{SupportedPoints: []byte{
-					0x00,
-				}},
-				&utls.SessionTicketExtension{},
+				&utls.SupportedPointsExtension{SupportedPoints: []byte{0x00}},
 				&utls.ALPNExtension{AlpnProtocols: []string{"h2", "http/1.1"}},
-				&utls.StatusRequestExtension{},
 				&utls.SignatureAlgorithmsExtension{SupportedSignatureAlgorithms: []utls.SignatureScheme{
 					utls.ECDSAWithP256AndSHA256,
 					utls.PSSWithSHA256,
 					utls.PKCS1WithSHA256,
-					utls.ECDSAWithP384AndSHA384,
-					utls.PSSWithSHA384,
-					utls.PKCS1WithSHA384,
-					utls.PSSWithSHA512,
-					utls.PKCS1WithSHA512,
 				}},
-				&utls.SCTExtension{},
-				&utls.KeyShareExtension{[]utls.KeyShare{
-					{Group: utls.CurveID(utls.GREASE_PLACEHOLDER), Data: []byte{0}},
-					{Group: utls.X25519MLKEM768},
+				&utls.KeyShareExtension{KeyShares: []utls.KeyShare{
 					{Group: utls.X25519},
 				}},
-				&utls.PSKKeyExchangeModesExtension{[]uint8{
-					utls.PskModeDHE,
-				}},
-				&utls.SupportedVersionsExtension{[]uint16{
-					utls.GREASE_PLACEHOLDER,
+				&utls.SupportedVersionsExtension{Versions: []uint16{
 					utls.VersionTLS13,
 					utls.VersionTLS12,
 				}},
-				&utls.UtlsCompressCertExtension{[]utls.CertCompressionAlgo{
-					utls.CertCompressionBrotli,
-				}},
-				&utls.ApplicationSettingsExtensionNew{SupportedProtocols: []string{"h2"}},
-				utls.BoringGREASEECH(),
-				&utls.UtlsGREASEExtension{},
-			}),
+			},
 		}
 	})
-	if _, err := c.R().Get("https://tls.browserleaks.com/json"); err != nil {
-		t.Errorf("TestSetTLSFingerprintSpec failed: %v", err)
+	if _, err := c.R().Get("/"); err != nil {
+		t.Errorf("TestSetTLSFingerprintSpec failed on first handshake: %v", err)
 	}
-	if _, err := c.R().Get("https://tools.scrapfly.io/api/fp/ja3"); err != nil {
-		t.Errorf("TestSetTLSFingerprintSpec failed: %v", err)
+	if _, err := c.R().Get(server1.URL); err != nil {
+		t.Errorf("TestSetTLSFingerprintSpec failed on consecutive handshake to different host: %v", err)
 	}
 }
